6f59a0d3e8b2074974471ef168b676d4650d375e
Patrick Cramer <pocramer14@tamu.edu>
2017-11-27 18:16:57 -0600

    Added MotionSensor.java and DoorSensor.java

---
+++ MotionSensor.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.IOException;
+import java.io.PrintStream;
+
+public class MotionSensor extends Thread{
+	
+	public void run(){
+		try{
+			
+		}catch(Exception e){
+			//error handling here
+		}
+	}
+}
